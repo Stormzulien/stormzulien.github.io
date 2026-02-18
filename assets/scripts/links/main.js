7760a68db22db83f2a33f57c8e1642984e570766
Stormzulien <jayvadgama2009@gmail.com>
2026-02-18 03:07:09 +0000

    Add files via upload

---
+++ assets/scripts/links/main.js
@@ -0,0 +1,213 @@
+/*
+  Details:
+    Source: "https://stormzulien.github.io/"
+
+    By Stormzulien (Jay)
+
+    Started: 09/02/2026,
+    Last Updated: 17/02/2026
+    
+    (Format: DD/MM/YYYY)
+
+  Notes:
+*/
+
+"use strict";
+
+import doc from "../doc.js";
+import globalConfig from "../../../data/global_config.js";
+import links from "../../../data/links/links.js";
+import formatUrl from "../utils/format_url.js";
+
+const sources = {
+  dataDir: "../data/",
+  assetsDir: "../assets/",
+  socialIconsDir: "media/images/icons/social_icons/",
+  homeIcon: {
+    light: "../assets/media/images/icons/ui/home_icon_black.svg",
+    dark: "../assets/media/images/icons/ui/home_icon_white.svg"
+  },
+  twitX: {
+    twitter: "twitter.svg",
+    twitterX: "twitter_x.svg"
+  }
+}
+
+const linksList = doc.qs("#links-list");
+
+const footerContent = doc.qs("footer").innerHTML;
+const creepyFooterContent = "417e4705aee1415f8583243b8c403af3";
+
+if (globalConfig.customLinkStyles) {
+  doc.head.innerHTML += `<link rel="stylesheet" href="${sources.dataDir}links/links_custom_styles.css">`;
+}
+
+function generateClouds() {
+  const decoClouds = doc.qs("#deco-clouds");
+
+  let cloudsHTML = "";
+  for (let i = 0; i < 6; i++) {
+    cloudsHTML += `
+      <img id="cloud-${i}" draggable="false" src="${sources.assetsDir}media/images/clouds/cloud_${i}.svg">
+    `;
+  }
+  decoClouds.innerHTML = cloudsHTML;
+}
+
+generateClouds();
+
+links.forEach(link => {
+  let linkHTML = "";
+
+  if (link.shown) {
+    const formattedUrl = formatUrl(link.url)
+
+    let lightIcon = link.icon.fileName || "default.svg";;
+    let darkIcon = link.icon.darkTheme || lightIcon;
+
+    linkHTML = `
+      <li>
+        <a
+          tabindex="0" 
+          class="social-link ${link.type === "copy" ? "js-link-copy" : ""} ${link.customStylesClass || ""}" 
+          id="${link.elemId}" 
+          ${link.type === "link" ? `href="${formattedUrl}" target="_blank"` : ""} 
+          title="${link.title || `Go to &#x22;${formattedUrl}&#x22;`}"
+        >
+          <div class="social-icon-container">
+            <img class="social-icon" draggable="false" src="${sources.assetsDir}${sources.socialIconsDir}${isDarkTheme() ? darkIcon : lightIcon}" alt="${link.icon.altText || 'Icon'}">
+          </div>
+          <div class="content-right">
+            <span class="platform-name">${link.name}</span>
+            <span class="username">${link.username}</span>
+          </div>
+        </a>
+      </li>
+    `;
+
+    linksList.innerHTML += linkHTML;
+  }
+});
+
+const jsLinkCopy = doc.qsa(".js-link-copy");
+
+jsLinkCopy.forEach(element => {
+  const elementId = element.id;
+
+  let copyContent = "";
+  let copyMsg = "Copied!";
+  let initialContent = "";
+
+  links.forEach(link => {
+    if (link.elemId === elementId) {
+      copyContent = link.copyContent;
+      copyMsg = link.copyMsg;
+      initialContent = link.username;
+    }
+  });
+
+  element.addEventListener("click", () => {
+    const elem = doc.qs(`#${elementId} .username`);
+    navigator.clipboard.writeText(copyContent);
+    elem.classList.add("copy-msg");
+    elem.innerText = copyMsg;
+
+    setTimeout(() => {
+      elem.innerText = initialContent;
+      elem.classList.remove("copy-msg");
+    }, 2000);
+  });
+});
+
+if (globalConfig.xitter) {
+  const twitX = doc.qs("a#link-twitter");
+  const twitXLogo = doc.qs("a#link-twitter img.social-icon");
+  const twitXPlatformName = doc.qs("a#link-twitter span.platform-name");
+
+  const hellSound = doc.qs("#hell-ambient-audio");
+  // Sound Effect by "https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=23836" freesound_community from Pixabay
+
+  hellSound.loop = true;
+
+  doc.body.addEventListener("keydown", (event) => {
+    if (event.key === "x") {
+      twitXLogo.src = `${sources.assetsDir}${sources.socialIconsDir}${sources.twitX.twitterX}`;
+      twitXPlatformName.innerText = "X";
+      doc.footer.innerHTML = `${String.fromCharCode(Math.floor(Math.random() * (10000 - 100 + 1) + 100))} ${creepyFooterContent} ඞ`;
+      twitX.classList.add("elon");
+      doc.body.classList.add("elon-effect");
+      hellSound.play();
+    }
+  });
+
+  doc.body.addEventListener("keyup", (event) => {
+    if (event.key === "x") {
+      twitXLogo.src = `${sources.assetsDir}${sources.socialIconsDir}${sources.twitX.twitter}`;
+      twitXPlatformName.innerText = "Twitter";
+      doc.footer.innerHTML = footerContent;
+      twitX.classList.remove("elon");
+      doc.body.classList.remove("elon-effect");
+      hellSound.pause();
+    }
+  });
+}
+
+const homeLnkImg = document.createElement("img");
+  homeLnkImg.alt = "Home icon";
+  homeLnkImg.draggable = "false";
+  homeLnkImg.src = isDarkTheme() ? sources.homeIcon.dark : sources.homeIcon.light;
+
+doc.qs("#home-lnk").appendChild(homeLnkImg);
+
+// Change icon themes
+
+function switchIconTheme(theme) {
+
+  // Link list
+
+  doc.qsa(".social-link").forEach(element => {
+    const elementId = element.id;
+
+    let lightIcon;
+    let darkIcon;
+    links.forEach(link => {
+      if (link.elemId === elementId) {
+        lightIcon = link.icon.fileName || "default.svg";
+        darkIcon = link.icon.darkTheme || lightIcon;
+      }
+    });
+
+    const icon = doc.qs(`#${elementId} img.social-icon`);
+    icon.src = `${sources.assetsDir}${sources.socialIconsDir}${theme ? darkIcon : lightIcon}`;
+  });
+
+  // Home icon
+
+  const homeIcon = doc.qs("#home-lnk img");
+
+  homeIcon.src = theme ? sources.homeIcon.dark : sources.homeIcon.light;
+}
+
+function isDarkTheme() {
+  if (window.matchMedia) {
+    // Check if the dark-mode Media-Query matches
+    if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      // Dark
+      return true;
+    } else {
+      // Light
+      return false;
+    }
+  } else {
+    // Default (when Media-Queries are not supported)
+    console.error("Media Queries not supported.");
+    return;
+  }
+}
+
+// switchIconTheme(getCurrentTheme() ? true : false);
+
+matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", () => {
+    switchIconTheme(isDarkTheme());
+  });
---
+++ data/global_config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const globalConfig = {
+  customLinkStyles: !true,
+  linksOldSettings: {
+    redirectToNew: false,
+    showUpdateMsg: true
+  },
+  xitter: true
+};
+
+export default globalConfig;
